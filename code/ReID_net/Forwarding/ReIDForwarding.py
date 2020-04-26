3956f86fdc3ededd09ace89be650619f563feda5
GeniusGaryant <1546308416@qq.com>
2020-04-26 11:22:00 +0800

    Main update for current task

--- code/ReID_net/Forwarding/ReIDForwarding.py
+++ code/ReID_net/Forwarding/ReIDForwarding.py
@@ -2,98 +2,220 @@ import numpy as np
 import os
 import time
 from .Forwarder import Forwarder
-from ReID_net.Log import log
+from Log import log
 import glob
 import json
+from collections import OrderedDict
+
 
 class ReIDForwarder(Forwarder):
-  def __init__(self, engine):
-    super(ReIDForwarder, self).__init__(engine)
-    self.network = None
-    self.data = None
 
-  def get_all_latents_from_network(self, y_softmax):
-    tag = self.network.tags
-    n_total = self.data.num_examples_per_epoch()
-    n_processed = 0
-    ys = []
-    tags = []
-    while n_processed < n_total:
-      start = time.time()
-      y_val, tag_val = self.session.run([y_softmax, tag])
-      y_val = y_val[0]
-      curr_size = y_val.shape[0]
-      for i in range(curr_size):
-        ys.append(y_val[i])
-        tags.append(tag_val[i].decode('utf-8'))
-      n_processed += curr_size
-      print(n_processed, "/", n_total, " elapsed: ", time.time() - start)
-    self.export_data(ys, tags)
+    def __init__(self, engine):
+        super(ReIDForwarder, self).__init__(engine)
+        self.network = None
+        self.data = None
 
-  def export_data(self, ys, tags):
-    print("EXPORTING DATA")
+    def get_all_latents_from_network(self, y_softmax):
+        tag = self.network.tags
+        n_total = self.data.num_examples_per_epoch()
+        n_processed = 0
+        ys = []
+        tags = []
+        start_all = time.time()
+        while n_processed < n_total:
+            start = time.time()
+            y_val, tag_val = self.session.run([y_softmax, tag])
+            y_val = y_val[0]
+            curr_size = y_val.shape[0]
+            for i in range(curr_size):
+                ys.append(y_val[i])
+                tags.append(tag_val[i].decode('utf-8'))
+            n_processed += curr_size
+            print(n_processed, "/", n_total, " elapsed: ", time.time() - start)
+        print("Process finished, took {} sec".format(time.time() - start_all))
 
-    # old_proposal_directory = "/home/luiten/vision/PReMVOS/first_frame_no_ReID/%s/"
-    # new_proposal_dir = "/home/luiten/vision/PReMVOS/first_frame_final/%s/"
+        self.export_data(ys, tags)
 
-    old_proposal_directory = self.config.str("bb_input_dir", None)
-    new_proposal_dir = self.config.str("output_dir", None)
+    def export_data(self, ys, tags):
+        print("EXPORTING DATA")
 
-    # old_proposal_directory = "/home/luiten/vision/PReMVOS/proposals_with_flow/%s/"
-    # new_proposal_dir = "/home/luiten/vision/PReMVOS/final_proposals/%s/"
+        old_proposal_directory = self.config.str("bb_input_dir", None)
+        # new_proposal_dir = self.config.str("output_dir", None)
 
-    # old_proposal_directory = "/home/luiten/vision/PReMVOS/post_proposal_expansion_json_with_flow/%s/"
-    # new_proposal_dir = "/home/luiten/vision/PReMVOS/expanded_final_props/%s/"
-    # sets = ['test-challenge/', 'val/', 'test-dev/']
-    # sets = ['val/',]
-    all_proposals = dict()
+        all_proposals = {}
+        # Read in all proposals
+        folders = sorted(glob.glob(old_proposal_directory + '*/'))
+        for folder in folders:
+            seq = folder.split('/')[-2]
+            name = seq
+            files = sorted(
+                glob.glob(old_proposal_directory + name + "/*.json"))
 
-    # Read in all proposals
-    # for set_id, set in enumerate(sets):
-    folders = sorted(glob.glob(old_proposal_directory + '*/'))
-    for folder in folders:
-      seq = folder.split('/')[-2]
-      name = seq
-      files = sorted(glob.glob(old_proposal_directory + name + "/*.json"))
-      for file in files:
-        timestep = file.split('/')[-1].split('.json')[0]
+            all_proposals[name] = {}
+            all_proposals[name]["00"] = {}
 
-        # Get proposals:
-        with open(file, "r") as f:
-          proposals = json.load(f)
-        all_proposals[name+'/'+timestep] = proposals
+            for file in files:
+                timestep = file.split('/')[-1].split('.json')[0]
+                # Get proposals:
+                with open(file, "r") as f:
+                    # [{'bbox': [945.0, 293.0, 52.0, 28.0]}]
+                    proposals = json.load(f)
+                    the_bbox = proposals[0]["bbox"]
+                    print("the_bbox: {}".format(the_bbox))
+                    if the_bbox == [0.0, 0.0, 0.0, 0.0]:
+                        print("________timestep: {}".format(timestep))
+                
+                all_proposals[name]["00"][timestep] = []
 
-    print("READ IN ALL PROPOSALS")
+                # all_proposals[name+'/'+timestep] = proposals
 
-    # Insert embeddings into proposals
-    for y,tag in zip(ys,tags):
-      nametime,prop_id = tag.split('___')
-      prop_id = int(prop_id)
-      y = np.array(y).tolist()
-      all_proposals[nametime][prop_id]["ReID"] = y
+        print("READ IN ALL PROPOSALS")
 
-    print("INSERTED EMBEDDINGS")
+        # Insert embeddings into proposals
+        for y, tag in zip(ys, tags):
+            nametime, prop_id = tag.split('___')
+            name, timestep = nametime.split("/")
+            # prop_id = int(prop_id)
+            y = np.array(y).tolist()
+            all_proposals[name]["00"][timestep] = y
 
-    # Save out to file
-    # for set_id, set in enumerate(sets):
-    folders = sorted(glob.glob(old_proposal_directory + '*/'))
-    for folder in folders:
-      seq = folder.split('/')[-2]
-      name = seq
-      files = sorted(glob.glob(old_proposal_directory + name + "/*.json"))
-      for file in files:
-        timestep = file.split('/')[-1].split('.json')[0]
+        print("INSERTED EMBEDDINGS")
 
-        # Save new proposals:
-        new_file = new_proposal_dir + name + "/" + timestep + ".json"
-        if not os.path.exists(new_proposal_dir + name):
-          os.makedirs(new_proposal_dir + name)
-        with open(new_file, 'w') as f:
-          json.dump(all_proposals[name+'/'+timestep], f)
+        # with open("/home/zjh/PReMVOS/final_lasot_2.json", "w") as f:
+        #     final = {"LASOT": all_proposals}
+        #     # final = all_proposals
+        #     json.dump(final, f)
+        
+        print("SAVED ALL PROPOSALS WITH REID VECTOR")
 
-  def forward(self, network, data, save_results=True, save_logits=False):
-    self.network = network
-    self.data = data
+        # # Save out to file
+        # # for set_id, set in enumerate(sets):
+        # folders = sorted(glob.glob(old_proposal_directory + '*/'))
+        # for folder in folders:
+        #     seq = folder.split('/')[-2]
+        #     name = seq
+        #     files = sorted(
+        #         glob.glob(old_proposal_directory + name + "/*.json"))
+        #     for file in files:
+        #         timestep = file.split('/')[-1].split('.json')[0]
 
-    output = self.network.get_output_layer().outputs
-    self.get_all_latents_from_network(output)
+        #         # Save new proposals:
+        #         new_file = new_proposal_dir + name + "/" + timestep + ".json"
+        #         if not os.path.exists(new_proposal_dir + name):
+        #             os.makedirs(new_proposal_dir + name)
+        #         with open(new_file, 'w') as f:
+        #             json.dump(all_proposals[name+'/'+timestep], f)
+
+    """
+    Final dict structure:
+    {
+        "visdrone": {
+            "seq1": {
+                "00": {
+                    "query1": [.......]
+                }
+            }
+        },
+        "got-10k": {
+            ......
+        }
+    }
+    """
+    def get_latents_and_export_data(self, y_softmax, seq_path):
+        print("EXPORTING DATA")
+
+        old_proposal_directory = self.config.str("bb_input_dir", None)
+        all_proposals = {}
+
+        name = seq_path
+        all_proposals[name] = {}
+        files = sorted(
+            glob.glob(old_proposal_directory + seq_path + "/*.json"))
+        for file in files:
+            file_name = file.split('/')[-1].split('.json')[0]
+            ob_id = file_name.split('.')[1]
+            timestep = file_name.split('.')[0]
+            # timestep = str(int(timestep)) # 去掉trackingnet序列中前面的0
+            all_proposals[name][ob_id] = {}
+            # print("all_proposals[{}][{}][{}]: initialized".format(name, ob_id, timestep))
+
+        tag = self.network.tags
+        n_total = self.data.num_examples_per_epoch()
+        n_processed = 0
+        start_all = time.time()
+
+        # problem_frame = []
+        while n_processed < n_total:
+            start = time.time()
+            try:
+                y_val, tag_val = self.session.run([y_softmax, tag])
+                y_val = y_val[0]
+                curr_size = y_val.shape[0]
+                for i in range(curr_size):
+                    nametime, _ = tag_val[i].decode('utf-8').split("___")
+                    file_name = nametime.split("/")[-1]
+                    ob_id = file_name.split('.')[1]
+                    timestep = file_name.split('.')[0]
+                    y = np.array(y_val[i]).tolist()
+                    all_proposals[name][ob_id][timestep] = y
+                n_processed += curr_size
+            except:
+                print("Problem: {}".format(file_name))
+                all_proposals[name][ob_id][timestep] = [0.] * 128
+                n_processed += 1
+
+            # try:
+            #     y_val, tag_val = self.session.run([y_softmax, tag])
+            #     y_val = y_val[0]
+            #     curr_size = y_val.shape[0]
+            #     for i in range(curr_size):
+            #         nametime, _ = tag_val[i].decode('utf-8').split("___")
+            #         file_name = nametime.split("/")[-1]
+            #         ob_id = file_name.split('.')[1]
+            #         timestep = file_name.split('.')[0]
+            #         y = np.array(y_val[i]).tolist()
+            #         all_proposals[name][ob_id][timestep] = y
+            #     n_processed += curr_size
+            # except:
+            #     print("Problem here.")
+            #     import pdb; pdb.set_trace()
+
+            #     try:
+            #         all_proposals[name][ob_id][str(n_processed).zfill(6)] = y
+            #     except:
+            #         print("From the first frame...")
+            #         all_proposals[name][ob_id][str(n_processed).zfill(6)] = []
+            #         problem_frame.append(n_processed)
+            #     n_processed += 1
+
+            print(n_processed, "/", n_total, " elapsed: ", time.time() - start)
+        
+        # debug到这里
+        # if problem_frame:
+        #     print("Circle finished, patching the problem frame..")
+        #     for i in problem_frame[::-1]:
+        #         print("Patching frame {}".format(i+1))
+        #         all_proposals[seq_path]["00"][str(i+1).zfill(6)] = all_proposals[seq_path]["00"][str(i+2).zfill(6)]
+        # else:
+        #     print("First Frame is ok.")
+
+        print("Process finished, took {} sec".format(time.time() - start_all))
+
+        dataset_name = "lasot"
+        seq_dir = ""
+        for sub_path in seq_path.split("/")[:-1]:
+            seq_dir += sub_path+"/"
+        save_dir = "/home/zjh/PReMVOS/score_" + dataset_name + "/" + seq_dir
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        with open(save_dir + seq_path.split("/")[-1] + ".json", "w") as f:
+            final = all_proposals
+            json.dump(final, f)
+            print("SAVED ALL PROPOSALS WITH REID VECTOR\n\n\n")
+
+
+    def forward(self, network, data, seq_path, save_results=True, save_logits=False):
+        self.network = network
+        self.data = data
+        output = self.network.get_output_layer().outputs
+        self.get_latents_and_export_data(output, seq_path)
--- code/ReID_net/main.py
+++ code/ReID_net/main.py
@@ -1,41 +1,103 @@
 #!/usr/bin/env python
 import sys
 import os
+import time
+import json
 
-from ReID_net.Engine import Engine
-from ReID_net.Config import Config
-from ReID_net.Log import log
+from Engine import Engine
+from Config import Config
+from Log import log
 import tensorflow as tf
 
 
 def init_log(config):
-  log_dir = config.dir("log_dir", "logs")
-  model = config.str("model")
-  filename = log_dir + model + ".log"
-  verbosity = config.int("log_verbosity", 3)
-  log.initialize([filename], [verbosity], [])
+    log_dir = config.dir("log_dir", "logs")
+    model = config.str("model")
+    filename = log_dir + model + ".log"
+    verbosity = config.int("log_verbosity", 3)
+    log.initialize([filename], [verbosity], [])
 
 
 def main(_):
-  assert len(sys.argv) == 2, "usage: main.py <config>"
-  config_path = sys.argv[1]
-  assert os.path.exists(config_path), config_path
-  try:
-    config = Config(config_path)
-  except ValueError as e:
-    print("Malformed config file:", e)
-    return -1
-  init_log(config)
-  config.initialize()
-  #dump the config into the log
-  print(open(config_path).read())
-  engine = Engine(config)
-  engine.run()
+    assert len(sys.argv) == 3, "usage: main.py <config>"
+    config_path = sys.argv[1]
+    seq_name = sys.argv[2]
+    assert os.path.exists(config_path), config_path
+
+    try:
+        config = Config(config_path)
+    except ValueError as e:
+        print("Malformed config file:", e)
+        return -1
+    # init_log(config)
+    config.initialize()
+
+    dataset_name = "lasot"
+    # for coco:  seq_name: 'train2017/000000287469'
+    # for det:   seq_name: 'a/n02028035_10675'
+    # for vid:   seq_name: 'd/ILSVRC2015_train_01085000'
+    # for yt_bb: seq_name: 'train0021/0/CbO2dReBn1o'
+
+    seq_dir = ""
+    for sub_path in seq_name.split("/")[:-1]:
+        seq_dir += sub_path+"/"
+    save_dir = "/home/zjh/PReMVOS/score_" + dataset_name + "/" + seq_dir
+    if not os.path.exists(save_dir + seq_name.split("/")[-1] + ".json"):
+        start = time.time()
+        print("Running seq_file: {}".format(seq_name))
+        engine = Engine(config, seq_name)
+        engine.run()
+        print("seq_file {} finished, took {} sec".format(seq_name, time.time()-start))
+    else:
+        print("The seq_file {} already exists, move to the next..".format(seq_name))
+
+
+    # if dataset_name == "coco":
+    #     anno_name = "/home/zjh/PReMVOS/data/training_dataset/coco/train2017.json"
+    # elif dataset_name == "det":
+    #     anno_name = "/home/zjh/PReMVOS/data/training_dataset/det/train.json"
+    # elif dataset_name == "vid":
+    #     anno_name = "/home/zjh/PReMVOS/data/training_dataset/vid/train.json"
+    # elif dataset_name == "yt_bb":
+    #     anno_name = "/home/zjh/PReMVOS/data/training_dataset/yt_bb/train.json"
+    # elif dataset_name == "lasot":
+    #     anno_name = "/home/zjh/PReMVOS/data/training_dataset/lasot/train.json"
+    # elif dataset_name == "got-10k":
+    #     anno_name = "/home/zjh/PReMVOS/data/training_dataset/got-10k/train.json"
+    # elif dataset_name == "trackingnet":
+    #     anno_name = "/home/zjh/PReMVOS/data/training_dataset/trackingnet/train.json"
+    # # elif dataset_name == "visdrone":
+    # #     anno_name = "/home/zjh/PReMVOS/data/training_dataset/visdrone/train.json"
+    # else:
+    #     raise AssertionError
+
+    # f = open(anno_name, "r")
+    # dataset_dict = json.load(f)
+    # for seq_name in dataset_dict.keys():
+    #     # for coco:  seq_name: 'train2017/000000287469'
+    #     # for det:   seq_name: 'a/n02028035_10675'
+    #     # for vid:   seq_name: 'd/ILSVRC2015_train_01085000'
+    #     # for yt_bb: seq_name: 'train0021/0/CbO2dReBn1o'
+
+    #     seq_dir = ""
+    #     for sub_path in seq_name.split("/")[:-1]:
+    #         seq_dir += sub_path+"/"
+    #     save_dir = "/home/zjh/PReMVOS/score_" + dataset_name + "/" + seq_dir
+    #     if not os.path.exists(save_dir + seq_name.split("/")[-1] + ".json"):
+    #         start = time.time()
+    #         print("Running seq_file: {}".format(seq_name))
+    #         engine = Engine(config, seq_name)
+    #         engine.run()
+    #         print("seq_file {} finished, took {} sec".format(seq_name, time.time()-start))
+    #     else:
+    #         print("The seq_file {} already exists, move to the next..".format(seq_name))
+    # f.close()
+
 
 if __name__ == '__main__':
-  #for profiling. Note however that this will not be useful for the execution of the tensorflow graph,
-  #only for stuff like initialization including creation of the graph, loading of weights, etc.
-  #import cProfile
-  #cProfile.run("tf.app.run(main)", sort="tottime")
+    # for profiling. Note however that this will not be useful for the execution of the tensorflow graph,
+    # only for stuff like initialization including creation of the graph, loading of weights, etc.
+    # import cProfile
+    # cProfile.run("tf.app.run(main)", sort="tottime")
 
-  tf.app.run(main)
+    tf.app.run(main)
