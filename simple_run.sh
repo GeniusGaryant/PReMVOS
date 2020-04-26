3c1c83611991f65e8c0980693aada9c6faa00e8d
GeniusGaryant <1546308416@qq.com>
2020-04-26 11:22:30 +0800

    update running script

--- simple_run.sh
+++ simple_run.sh
@@ -1,80 +1,56 @@
 #!/bin/bash
 
-# Download and unzip DAVIS train/val if it is not already there (~800MB)
-if [ ! -d "./data/DAVIS" ]; then
-  echo "################# DOWNLOADING DAVIS DATA #################"
-  wget -P ./data https://data.vision.ee.ethz.ch/csergi/share/davis/DAVIS-2017-trainval-480p.zip
-  echo "################# UNZIPPING DAVIS DATA #################"
-  unzip ./data/DAVIS-2017-trainval-480p.zip -d ./data
-fi
-
-# Download and unzip weights if they are not already there  (~3GB)
-if [ ! -d "./weights/PReMVOS_weights" ]; then
-  echo "################# DOWNLOADING WEIGHTS #################"
-  wget -P ./weights https://www.vision.rwth-aachen.de/media/resource_files/PReMVOS_weights.zip
-  echo "################# UNZIPPING WEIGHTS #################"
-  unzip ./weights/PReMVOS_weights.zip -d ./weights
-fi
-
-# Run method on sequences defined in ./seq_to_run.txt
-
-# Calculate optical flow
-FLOW_LOC=./output/intermediate/flow
-if [ ! -d "$FLOW_LOC" ]; then
-  echo "################# GENERATING FLOW #################"
-  ./code/optical_flow_net-PWC-Net/script_pwc_multi.py
-fi
-
-# Generate general proposals
-GENERAL_PROP_LOC=./output/intermediate/general_proposals
-if [ ! -d "$GENERAL_PROP_LOC" ]; then
-  echo "################# GENERATING GENERAL PROPOSALS #################"
-  PROP_NET_GENERAL_WEIGHTS=./weights/PReMVOS_weights/proposal_net/general_weights/proposal_general_weights
-  ./code/proposal_net/train.py --forward "$GENERAL_PROP_LOC" --agnostic --second_head --forward_dataset DAVIS --load "$PROP_NET_GENERAL_WEIGHTS" --davis_name "$PWD/seq_to_run.txt"
-fi
-
-# Generate specific proposals
-SPECIFIC_PROP_LOC=./output/intermediate/specific_proposals
-if [ ! -d "$SPECIFIC_PROP_LOC" ]; then
-  echo "################# GENERATING SPECIFIC PROPOSALS #################"
-  PROP_NET_SPECIFIC_WEIGHTS=./weights/PReMVOS_weights/proposal_net/specific_weights/proposal_specific_weights
-  ./code/proposal_net/train.py --forward "$SPECIFIC_PROP_LOC" --agnostic --second_head --forward_dataset DAVIS --load "$PROP_NET_SPECIFIC_WEIGHTS" --davis_name "$PWD/seq_to_run.txt"
-fi
-
-# Combine general and specific proposals
-COMBINED_PROP_LOC=./output/intermediate/combined_proposals
-if [ ! -d "$COMBINED_PROP_LOC" ]; then
-  echo "################# GENERATING COMBINED PROPOSALS #################"
-  ./code/proposal_net/combine_general_and_specific.py
-fi
-
-# Refine proposals
-REFINED_PROP_LOC=./output/intermediate/refined_proposals
-if [ ! -d "$REFINED_PROP_LOC" ]; then
-  echo "################# GENERATING REFINED PROPOSALS #################"
-  cd code
-  REFINEMENT_CONFIG=./refinement_net/configs/run
-  echo "$REFINEMENT_CONFIG"
-  ./refinement_net/main.py "$REFINEMENT_CONFIG"
-  cd ..
-fi
-
 # Add ReID to proposals
-ReID_PROP_LOC=./output/intermediate/ReID_proposals
-if [ ! -d "$ReID_PROP_LOC" ]; then
-  echo "################# GENERATING ReID PROPOSALS #################"
-  cd code
-  ReID_CONFIG=./ReID_net/configs/run
-  echo "$ReID_CONFIG"
-  ./ReID_net/main.py "$ReID_CONFIG"
-  cd ..
-fi
+DATASET=lasot
+GPU_NUMBER=1
+IMG_DIR=/home/zjh/PReMVOS/split_$DATASET/$GPU_NUMBER
+files=$(ls $IMG_DIR | cut -d . -f1)
+for i in $files
+do
+    echo "################# GENERATING $i PROPOSALS #################"
+    cd code
+    ReID_CONFIG=./ReID_net/configs/run
+    echo "$ReID_CONFIG"
+    CUDA_VISIBLE_DEVICES=$GPU_NUMBER ./ReID_net/main.py "$ReID_CONFIG" $i
+    cd ..
+done
 
-# Run merging algorithm
-MERGE_OUT_LOC=./output/final
-if [ ! -d "$MERGE_OUT_LOC" ]; then
-  echo "################# Merging #################"
-  cd code
-  ./MergeTrack/merge.py
-  cd ..
-fi
+
+
+# # Problem seq
+# # seq="GOT-10k_Train_008628"
+# seq="GOT-10k_Train_008630"
+# # seq="GOT-10k_Train_009058"
+# echo "################# DEBUG $seq #################"
+# cd code
+# ReID_CONFIG=./ReID_net/configs/run
+# # echo "$ReID_CONFIG"
+# CUDA_VISIBLE_DEVICES=$GPU_NUMBER ./ReID_net/main.py "$ReID_CONFIG" $seq
+# cd ..
+
+
+
+# IMG_DIR=/home/zjh/PReMVOS/split_trackingnet/1
+# dir=$(ls -l $IMG_DIR |awk '/^d/ {print $NF}')
+# for i in $dir
+# do
+#   echo $i
+#   # echo "################# GENERATING ReID PROPOSALS #################"
+#   # cd code
+#   # ReID_CONFIG=./ReID_net/configs/run
+#   # echo "$ReID_CONFIG"
+#   # CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7,8 ./ReID_net/main.py "$ReID_CONFIG" $i
+#   # cd ..
+# done
+
+
+
+# if [ ! -d "$ReID_PROP_LOC" ]; then
+#   echo "################# GENERATING ReID PROPOSALS #################"
+#   cd code
+#   ReID_CONFIG=./ReID_net/configs/run
+#   echo "$ReID_CONFIG"
+#   CUDA_VISIBLE_DEVICES=1 ./ReID_net/main.py "$ReID_CONFIG"
+#   cd ..
+# fi
+
